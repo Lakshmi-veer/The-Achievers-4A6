d88d19a3a1643c098b3feb7f19c1429fe9316603
Lakshmi-veer <talariveeralakshmi@gmail.com>
2025-07-30 14:08:12 +0530

    Adding all files

---
+++ activity1.py
@@ -0,0 +1,7 @@
+#print either  number is even orodd number
+number=int(input("enter your number"))
+if (number%2==0):
+    print("the number is even number")
+else:
+    print("the number is odd number")
+    
